71271b3eea51ba0cc9e94983a6b8e7788bc2102b
Andrey Kamenskiy <52415618+AndreyKamenskiy@users.noreply.github.com>
2020-03-11 21:02:57 +0300

    init

---
+++ task/src/tictactoe/Main.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Start project
+        Scanner scanner = new Scanner(System.in);
+// test git 
+    }
+}
